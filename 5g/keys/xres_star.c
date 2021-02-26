ec1bee5c39415a0e590419358f63efe5d12e971b
parrotshen <parrotshen@yahoo.com.tw>
2021-02-26 10:35:41 +0800

    XRES size could be 8 or 16

--- 5g/keys/xres_star.c
+++ 5g/keys/xres_star.c
@@ -33,6 +33,7 @@ uint8  xres[16] = {
  0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 };
 
+
 int main(void)
 {
     uint8  KEY[32];
@@ -44,30 +45,67 @@ int main(void)
     memcpy(KEY,    ck, 16);
     memcpy(KEY+16, ik, 16);
 
-    /* FC */
-    S[0]  = 0x6B;
-    /* P0 */
-    memcpy(S+1, snn, 32);
-    /* L0 */
-    S[33] = 0x00;
-    S[34] = 0x20;
-    /* P1 */
-    memcpy(S+35, _rand, 16);
-    /* L1 */
-    S[51] = 0x00;
-    S[52] = 0x10;
-    /* P2 */
-    memcpy(S+53, xres, 16);
-    /* L2 */
-    S[69] = 0x00;
-    S[70] = 0x10;
+    #if 0
+  
+    /* 8-byte XRES */
+    {
+        /* FC */
+        S[0]  = 0x6B;
+        /* P0 */
+        memcpy(S+1, snn, 32);
+        /* L0 */
+        S[33] = 0x00;
+        S[34] = 0x20;
+        /* P1 */
+        memcpy(S+35, _rand, 16);
+        /* L1 */
+        S[51] = 0x00;
+        S[52] = 0x10;
+        /* P2 */
+        memcpy(S+53, xres, 8);
+        /* L2 */
+        S[61] = 0x00;
+        S[62] = 0x08;
 
-    mem_dump("Key", KEY, 32);
-    mem_dump("S", S, 71);
+        mem_dump("Key", KEY, 32);
+        mem_dump("S", S, 63);
 
-    kdf(KEY, 32, S, 71, buf);
-    memcpy(XRES_STAR, buf+16, 16);
-    mem_dump("XRES*", XRES_STAR, 16);
+        kdf(KEY, 32, S, 63, buf);
+        memcpy(XRES_STAR, buf+16, 16);
+        mem_dump("XRES*", XRES_STAR, 16);
+    }
+
+    #else
+
+    /* 16-byte XRES */
+    {
+        /* FC */
+        S[0]  = 0x6B;
+        /* P0 */
+        memcpy(S+1, snn, 32);
+        /* L0 */
+        S[33] = 0x00;
+        S[34] = 0x20;
+        /* P1 */
+        memcpy(S+35, _rand, 16);
+        /* L1 */
+        S[51] = 0x00;
+        S[52] = 0x10;
+        /* P2 */
+        memcpy(S+53, xres, 16);
+        /* L2 */
+        S[69] = 0x00;
+        S[70] = 0x10;
+
+        mem_dump("Key", KEY, 32);
+        mem_dump("S", S, 71);
+
+        kdf(KEY, 32, S, 71, buf);
+        memcpy(XRES_STAR, buf+16, 16);
+        mem_dump("XRES*", XRES_STAR, 16);
+    }
+
+    #endif
 
 
     return 0;

0a3883db059dbdf5858e2c3cbab10a6f519ccb7b
parrotshen <parrotshen@yahoo.com.tw>
2021-12-13 09:44:42 +0800

    inline function

---
+++ inline/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+inline int test1(int i)
+{
+    int j = 0;
+
+_LABEL_T11:
+    j = i * i;
+
+_LABEL_T12:
+    printf("_LABEL_T11 at [1;33m%p[0m\n", &&_LABEL_T11);
+    printf("_LABEL_T12 at [1;33m%p[0m\n", &&_LABEL_T12);
+    return j;
+}
+
+int test2(int i)
+{
+    int j = 0;
+
+_LABEL_T21:
+    j = i * i;
+
+_LABEL_T22:
+    printf("_LABEL_T21 at [1;36m%p[0m\n", &&_LABEL_T21);
+    printf("_LABEL_T22 at [1;36m%p[0m\n", &&_LABEL_T22);
+    return j;
+}
+
+int main(void)
+{
+    int i = 9;
+
+_LABEL_M01:
+    test1( i );
+    printf("\n");
+
+_LABEL_M02:
+    test2( i );
+    printf("\n");
+
+_LABEL_M03:
+    test1( i );
+    printf("\n");
+
+_LABEL_M04:
+    test2( i );
+    printf("\n");
+
+_LABEL_M05:
+    printf("_LABEL_M01 at %p\n", &&_LABEL_M01);
+    printf("_LABEL_M02 at %p\n", &&_LABEL_M02);
+    printf("_LABEL_M03 at %p\n", &&_LABEL_M03);
+    printf("_LABEL_M04 at %p\n", &&_LABEL_M04);
+    printf("_LABEL_M05 at %p\n", &&_LABEL_M05);
+    return 0;
+}
+

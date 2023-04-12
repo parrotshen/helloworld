900db6b788e9303167a1779751079141892f0306
parrotshen <parrotshen@yahoo.com.tw>
2023-04-12 09:07:45 +0800

    Update cast.c

--- datatype/cast.c
+++ datatype/cast.c
@@ -18,8 +18,10 @@ void dump(unsigned char *pData, int size)
 int main(void)
 {
     char   val8;
+    short  val16;
     int    val32;
     double valfloat;
+    unsigned short dat16;
 
 
     printf("\n");
@@ -59,6 +61,33 @@ int main(void)
     printf("\n");
 
 
+        dat16 = 0xE802; val8 = (char)dat16;
+    printf("unsigned 16-bit to signed 8-bit\n");
+    printf("   %u ", dat16);
+    dump((void *)&dat16, sizeof(short));
+    printf("=> %d ", val8);
+    dump((void *)&val8, sizeof(char));
+    printf("\n");
+
+
+    dat16 = 0xE802; val16 = (short)dat16;
+    printf("unsigned 16-bit to signed 16-bit\n");
+    printf("   %u ", dat16);
+    dump((void *)&dat16, sizeof(short));
+    printf("=> %d ", val16);
+    dump((void *)&val16, sizeof(short));
+    printf("\n");
+
+
+    dat16 = 0xE802; val32 = (int)dat16;
+    printf("unsigned 16-bit to signed 32-bit\n");
+    printf("   %u ", dat16);
+    dump((void *)&dat16, sizeof(short));
+    printf("=> %d ", val32);
+    dump((void *)&val32, sizeof(int));
+    printf("\n");
+    
+    
     return 0;
 }
 
